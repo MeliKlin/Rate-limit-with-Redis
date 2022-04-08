ff9ee18efdb4f9a2c71e7781b8ebdcf194d294ad
Klinton Sae Hyung Lee <klinton.lee@mercadolivre.com>
2022-04-08 11:20:36 -0300

    chore: add initial spring boot run application

---
+++ src/main/java/com/practicing/crudforfun/CrudForFunApplication.java
@@ -0,0 +1,13 @@
+package com.practicing.crudforfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudForFunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudForFunApplication.class, args);
+    }
+
+}
