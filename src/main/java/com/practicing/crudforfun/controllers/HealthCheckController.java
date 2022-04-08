df69ee9e4839fbbab8d0622866254f8addfe7729
Klinton Sae Hyung Lee <klinton.lee@mercadolivre.com>
2022-04-08 16:35:19 -0300

    feat: add rate limit route

--- src/main/java/com/practicing/crudforfun/controllers/HealthCheckController.java
+++ src/main/java/com/practicing/crudforfun/controllers/HealthCheckController.java
@@ -1,6 +1,11 @@
 package com.practicing.crudforfun.controllers;
 
+import com.practicing.crudforfun.dbs.DatabaseManager;
+import com.practicing.crudforfun.dbs.RedisManager;
+import com.practicing.crudforfun.ratelimits.TokenBucket;
+import com.practicing.crudforfun.repositories.RateLimitRepository;
 import com.practicing.crudforfun.utils.GetClientIp;
+import org.redisson.api.RedissonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +14,30 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class HealthCheckController {
 
+    private final DatabaseManager<RedissonClient> redisManager;
+    private final RateLimitRepository rateLimitRepository;
+
+    public HealthCheckController() {
+        this.redisManager = new RedisManager();
+        this.rateLimitRepository = new RateLimitRepository(redisManager);
+    }
+
     @GetMapping("/get-ip")
     public String getIp(HttpServletRequest request) {
         return new GetClientIp().run(request);
+    }
+
+    @GetMapping("/rate-limit")
+    public String rateLimit(HttpServletRequest request) {
+        String clientIp = new GetClientIp().run(request);
+
+        int tokensLeft = new TokenBucket(rateLimitRepository)
+                .execute(clientIp, "rate-limit");
+
+        if (tokensLeft == 0) {
+            return "Calma jovem!!\n 429 - Too many requests";
+        }
+        return "Tokens left: " + tokensLeft;
     }
 
 }
