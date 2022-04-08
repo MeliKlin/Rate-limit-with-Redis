8a618455d719d5c965a79d22a61148bdc0bd467d
Klinton Sae Hyung Lee <klinton.lee@mercadolivre.com>
2022-04-08 11:27:24 -0300

    feat: add customer controller with create and find by id routes

---
+++ src/main/java/com/practicing/crudforfun/controllers/CustomerController.java
@@ -0,0 +1,42 @@
+package com.practicing.crudforfun.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.practicing.crudforfun.dtos.CreateCustomerDTO;
+import com.practicing.crudforfun.dbs.RedisManager;
+import com.practicing.crudforfun.entities.Customer;
+import com.practicing.crudforfun.repositories.CustomersRepository;
+import com.practicing.crudforfun.usecases.customers.CreateCustomer;
+import com.practicing.crudforfun.usecases.customers.FindCustomerById;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private final RedisManager redisManager;
+    private final ObjectMapper objectMapper;
+    private final CustomersRepository customersRepository;
+
+    public CustomerController() {
+        redisManager = new RedisManager();
+        objectMapper = new ObjectMapper();
+        this.customersRepository = new CustomersRepository(redisManager, objectMapper);
+    }
+
+    @PostMapping
+    public void teste(
+            @RequestBody CreateCustomerDTO customerDTO
+    ) {
+        if (customerDTO.isValid()) {
+            new CreateCustomer(customersRepository).execute(customerDTO);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public Customer teste2(@PathVariable UUID id) {
+        return new FindCustomerById(customersRepository).execute(id);
+    }
+
+}
